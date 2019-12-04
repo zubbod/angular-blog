0de9d31228a7637d358ed2fe2b6f5ea4ffda17e1
zb <zubbod@gmail.com>
2019-12-04 22:37:14 +0300

    create the post

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../../shared/models/post';
 
 @Component({
   selector: 'app-create-page',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePageComponent implements OnInit {
 
+  form: FormGroup
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      author: new FormControl('', Validators.required),
+      content: new FormControl('', Validators.required),
+    });
+  }
+
+  submit() {
+    let post = new Post();
+    post = this.form.getRawValue();
+    console.log(post);
+    
   }
 
 }
---
+++ src/app/shared/interfaces/i-post.ts
@@ -0,0 +1,7 @@
+export interface IPost {
+  title: string;
+  author: string;
+  content: string;
+  date: Date;
+  id?: number;
+}
